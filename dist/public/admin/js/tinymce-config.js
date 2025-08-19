6c61def16de4f7d83b13f42a014059a7797933fa
Tan Phat <btphat11321@gmail.com>
2025-08-19 10:54:13 +0700

    .

--- dist/public/admin/js/tinymce-config.js
+++
@@ -1,7 +0,0 @@
-tinymce.init({
-  selector: 'textarea[textarea-mce]',
-  plugins: 'lists link image table code help wordcount',
-  // upload tu tinymce len api de luu tren cloudinary de khong phai luu vao db
-  images_upload_url: '/admin/upload',
-  automatic_uploads: true,
-});
